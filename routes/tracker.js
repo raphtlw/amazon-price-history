ef46b0bf1ebf1714fab6dc5fc47714d21e313279
Raphael Tang <raphpb1912@gmail.com>
2020-02-03 20:06:41 +0800

    Bug fixed with server side downloading 🧹

--- routes/tracker.js
+++ routes/tracker.js
@@ -3,10 +3,17 @@ const request = require('request');
 const cheerio = require('cheerio');
 const download = require('image-downloader');
 const path = require('path');
+const fs = require('fs');
 
 const router = express.Router();
 
 router.get('/', async (req, res) => {
+  const tempDir = path.join(process.cwd(), 'public', 'temp');
+
+  if (!fs.existsSync(tempDir)) {
+    fs.mkdirSync(tempDir);
+  }
+
   // Parsing the URL in order for it to be used by cheerio
   // Converts the URL into a camelcamelcamel.com link
   let url = req.query.url;
@@ -18,9 +25,9 @@ router.get('/', async (req, res) => {
   // Final URL
   url = `https://camelcamelcamel.com/${name}/product/${id}`;
 
-  console.log(`URL: ${url}\n`);
+  console.log(`URL: ${url}`);
   console.log(`Item name: ${name}`);
-  console.log(`Item ID: ${id}\n`);
+  console.log(`Item ID: ${id}`);
   console.log(`Final URL: ${url}`);
 
   request(url, (error, response, html) => {
@@ -38,7 +45,7 @@ router.get('/', async (req, res) => {
       (() => {
         const options = {
           url: itemPriceChartUrl,
-          dest: path.join(__dirname, '..', 'public', 'images', 'chart.png'),
+          dest: path.join(tempDir, 'chart.png'),
         }
         download.image(options).then(({filename, image}) => {
           console.log(`Downloaded price chart to ${filename}`);
